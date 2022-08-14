c51f966a9cf3e4b7f08e9fe3afb0f275913d68a5
Abdullahi Oladimeji <abdullahioladimeji90@gmail.com>
2022-08-14 19:13:18 +0100

    Image query function added to server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Router, Request, Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -29,6 +30,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+app.get('/filterimage', async (req: Request, res: Response) => {
+  const{image_url}=req.query.image_url.toString();
+  if(!image_url) {
+    res.status(400).send('image url is required');
+  }
+
+  const filtered_image = await filterImageFromURL(image_url);
+
+  res.status(200).sendFile(filtered_image, () => {
+    deleteLocalFiles([filtered_image]);
+    
+  });
+
+});
   //! END @TODO1
   
   // Root Endpoint
