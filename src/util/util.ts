05874b46225240adb98eebc5d35828801664abcd
Abdullahi Oladimeji <abdullahioladimeji90@gmail.com>
2022-11-18 10:55:29 +0100

    edit

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,13 +1,7 @@
 import fs from "fs";
 import Jimp = require("jimp");
 
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
+
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
